5a8c57e9ba01adce927862b239ee7a7e7a221a57
Karoo13 <47706737+Karoo13@users.noreply.github.com>
2019-08-15 03:58:31 -0400

    corrected for recent osu update
    also rewrote a few of the set/read/fetch operations, added some more object options.

--- EditorReaderExample.cs
+++ EditorReaderExample.cs
@@ -9,9 +9,11 @@ public class EditorReaderExample
         Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
         
         EditorReader reader = new EditorReader();
-        reader.SetProcess("osu!", 0);
         reader.FetchAll();
-
+        
+        Console.WriteLine("oR" + reader.objectRadius);
+        Console.WriteLine("sO" + reader.stackOffset);
+        
         Console.WriteLine(reader.ContainingFolder);
         Console.WriteLine(reader.Filename);
         Console.WriteLine("HP" + reader.HPDrainRate);
@@ -20,22 +22,40 @@ public class EditorReaderExample
         Console.WriteLine("OD" + reader.OverallDifficulty);
         Console.WriteLine("SV" + reader.SliderMultiplier);
         Console.WriteLine("TR" + reader.SliderTickRate);
-
+        
         Console.WriteLine("Current Time:");
         Console.WriteLine(reader.EditorTime());
-
+        
         Console.WriteLine("Timing Points:");
         for (int i = 0; i < reader.numControlPoints; i++)
             Console.WriteLine(reader.controlPoints[i].ToString());
-
-        Console.WriteLine("Selected Hit Objects:");
-        for (int i = 0; i < reader.numObjects; i++)
-            if (reader.hitObjects[i].IsSelected)
-            {
-                reader.hitObjects[i].DeStack(reader.stackOffset);
-                Console.WriteLine(reader.hitObjects[i].ToString());
-            }
-
-        Console.ReadLine();
+        
+        Console.WriteLine("Bookmarks:");
+        for (int i = 0; i < reader.numBookmarks; i++)
+            Console.WriteLine(reader.bookmarks[i]);
+        
+        Console.WriteLine(reader.SnapPosition());
+        
+        //Console.WriteLine("Hit Objects:");
+        //for (int i = 0; i < reader.numObjects; i++)
+        //    Console.WriteLine(reader.hitObjects[i].ToString());
+        
+        while(true){
+            reader.FetchSelected();
+            Console.WriteLine("Selected Hit Objects:");
+            for (int i = 0; i < reader.numSelected; i++)
+                Console.WriteLine(reader.selectedObjects[i].ToString());
+            
+            //reader.FetchClipboard();
+            //Console.WriteLine("Copied Hit Objects:");
+            //for (int i = 0; i < reader.numClipboard; i++)
+            //    Console.WriteLine(reader.clipboardObjects[i].ToString());
+            
+            //Console.WriteLine("Hovered Hit Object:");
+            //if (reader.FetchHovered())
+            //    Console.WriteLine(reader.hoveredObject.ToString());
+            
+            Console.ReadLine();
+        }
     }
 }
